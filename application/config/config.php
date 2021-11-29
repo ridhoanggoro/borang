2da364cfca903b9ec3bbe9b4ed997a5e0d3e1fad
Ridho Anggoro <makhluktuhanpalingkeren@gmail.com>
2021-11-29 16:12:01 +0700

    migrasi engine export excel

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://e-borangft.univpancasila.ac.id/';
+$root = "http://".$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $root;
 
 /*
 |--------------------------------------------------------------------------
@@ -76,7 +78,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'english';
+$config['language']	= 'bahasa';
 
 /*
 |--------------------------------------------------------------------------
@@ -136,7 +138,7 @@ $config['subclass_prefix'] = 'MY_';
 | Note: This will NOT disable or override the CodeIgniter-specific
 |	autoloading (application/config/autoload.php)
 */
-$config['composer_autoload'] = FALSE;
+$config['composer_autoload'] = 'vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -379,7 +381,7 @@ $config['encryption_key'] = 'r1dh0anggor0';
 */
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
+$config['sess_expiration'] = 1800;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
--- application/helpers/my_helper.php
+++ application/helpers/my_helper.php
@@ -15,23 +15,118 @@ if ( ! function_exists('current_utc_date_time')){
    }
 
 
-if ( ! function_exists('encode_url')){
-   function encode_url($value){
-         $CI =& get_instance();
-         $CI->load->library('encrypt');
-         $enc_value=$CI->encrypt->encode($value);
-         $enc_value=str_replace(array('+', '/', '='), array('-', '_', '~'), $enc_value);
-         return $enc_value;
-       }
+   if ( ! function_exists('encode_url')){
+      function encode_url($string) {
+   
+         $output = false;
+         /*
+         * read security.ini file & get encryption_key | iv | encryption_mechanism value for generating encryption code
+         */        
+         $security       = parse_ini_file("security.ini");
+         $secret_key     = $security["encryption_key"];
+         $secret_iv      = $security["iv"];
+         $encrypt_method = $security["encryption_mechanism"];
+      
+         // hash
+         $key    = hash("sha256", $secret_key);
+      
+         // iv – encrypt method AES-256-CBC expects 16 bytes – else you will get a warning
+         $iv     = substr(hash("sha256", $secret_iv), 0, 16);
+      
+         //do the encryption given text/string/number
+         $result = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+         $output = base64_encode($result);
+         return $output;
+      }
    }
-
-
-if ( ! function_exists('decode_url')){
-   function decode_url($value){
-         $CI =& get_instance();
-         $CI->load->library('encrypt');
-         $dec_value=str_replace(array('-', '_', '~'), array('+', '/', '='), $value);
-         $dec_value=$CI->encrypt->decode($dec_value);
-         return $dec_value;
-       }
+   
+   if ( ! function_exists('decode_url')){
+      function decode_url($string) {
+         /*
+         * decryption process
+         *
+         *
+         */
+   
+         $output = false;
+         /*
+         * read security.ini file & get encryption_key | iv | encryption_mechanism value for generating encryption code
+         */
+      
+         $security       = parse_ini_file("security.ini");
+         $secret_key     = $security["encryption_key"];
+         $secret_iv      = $security["iv"];
+         $encrypt_method = $security["encryption_mechanism"];
+      
+         // hash
+         $key    = hash("sha256", $secret_key);
+      
+         // iv – encrypt method AES-256-CBC expects 16 bytes – else you will get a warning
+         $iv = substr(hash("sha256", $secret_iv), 0, 16);
+      
+         //do the decryption given text/string/number
+      
+         $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+         return $output;
+      }
+   }
+   
+   if (!function_exists('date_sql')) {
+      function date_sql($string){
+         $newdate = date("Y-m-d", strtotime($string));   
+         return($newdate);
+      }
+   }
+   
+   if ( ! function_exists('clean_format')){
+      function clean_format($string){
+         $clean_string = str_replace(',', '', $string);
+         return $clean_string;
+      }
+   }
+   
+   if ( ! function_exists('format_angka')){
+      function format_angka($string){
+         $angka = number_format($string, 2);
+         return $angka;
+      }
+   }
+   
+   if ( ! function_exists('persentase')){
+      function persentase($number, $divider){
+         if ($number != 0) {
+            $persen = number_format(($number / $divider) * 100, 2);
+         }
+         else {
+            $persen = 0.0;
+         }
+         return $persen;
+      }
+   }
+   
+   if (!function_exists('tanggal_indo')) {
+      function tanggal_indo($tanggal){
+         $bulan = array (
+            1 =>   'Januari',
+            'Februari',
+            'Maret',
+            'April',
+            'Mei',
+            'Juni',
+            'Juli',
+            'Agustus',
+            'September',
+            'Oktober',
+            'November',
+            'Desember'
+         );
+         $pecahkan = explode('-', $tanggal);
+         
+         // variabel pecahkan 0 = tanggal
+         // variabel pecahkan 1 = bulan
+         // variabel pecahkan 2 = tahun
+       
+         $result = $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0];
+         return $result;
+      }
    }
