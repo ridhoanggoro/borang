67e004bb6c82f1853631141602a5f21d89062f20
Ridho Anggoro <makhluktuhanpalingkeren@gmail.com>
2022-12-29 13:39:39 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -326,7 +326,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = 'r1dh0anggor0';
+$config['encryption_key'] = '@ri3W1b0wo';
 
 /*
 |--------------------------------------------------------------------------
